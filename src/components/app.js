af8144102a92347c95417402aa3634c1f3834595
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-12 19:12:40 -0800

    React | Functional-Components - Kuroash Esmaili

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,35 @@
 
 
+import React from 'react';
+import Table from './table';
+
+const students=[
+        {
+            name: 'Jordan',
+            course: 'OOP',
+            grade: 90,
+        },
+        {
+            name: 'Justen',
+            course: 'React',
+            grade: 86,
+        },
+        {
+            name: 'Capo',
+            course: 'Node.js',
+            grade: 89,
+        }
+        ];
+
+
+
+
+export default () =>{
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
+
