8a5bea13126889c34a26c5f0b1a4345a7e34b65c
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-16 17:37:59 -0800

    created render method from featureSet4

---
+++ src/components/format_time.js
@@ -0,0 +1,11 @@
+
+
+import React, {Component} from 'React';
+
+export default class FromatTime extends Component{
+    render(){
+        return(
+             <div>{this.props.elapsed}</div>
+        );
+    }
+}
--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -11,19 +11,38 @@ export default class Stopwatch extends Component{
         }
         this.start=this.start.bind(this);
         this.stop=this.stop.bind(this);
+        this.update=this.update.bind(this);
+        this.reset=this.reset.bind(this);
+    }
+    update(){
+        const {status, start} = this.state;
+        if(status==='running'){
+            this.setState({
+                elapsed: new Date().getTime() - start
+            });
+            setTimeout(this.update,10);
+        }
     }
     start(){
-        this.setstate={
+        this.setState({
             status:'running',
             start:new Date().getTime()
-        }
+        });
+        setTimeout(this.update,10);
+        // this.timer = setInterval(this.handleUpdate, 1000)
     }
     stop(){
-        this.setState={
+        this.setState({
             status:'stopped'
-        }
+        });
     }
-
+    reset(){
+        this.setState({
+            status: 'stopped',
+            start: null,
+            elapsed: 0 
+        })
+    }
     render(){
         const {elapsed,status}=this.state;
         return(
@@ -32,7 +51,8 @@ export default class Stopwatch extends Component{
             <p>{status}</p>
             <button onClick={this.start}>Start</button>
             <button onClick={this.stop}>Stop</button>
+            <button onClick={this.reset}>Reset</button>
         </div>
-        )
+        );
     }
 }
