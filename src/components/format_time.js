88309a8fa5a697d43538617c864efda27c386dec
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-17 16:19:40 -0800

    format time had an update

--- src/components/format_time.js
+++ src/components/format_time.js
@@ -13,6 +13,17 @@ export default class FromatTime extends Component{
             ms:0
         }
     }
+    componentWillReceiveProps(nextProps){
+        const {elapsed}=nextProps;
+        const sec = elapsed/1000;
+        const min = sec/60
+        this.setState({
+            hour:Math.floor(min/60),
+            min:Math.floor(min%60),
+            sec:Math.floor(sec%60),
+            ms:Math.floor(elapsed%100)
+        });
+    }
     render(){
         const {hour,min,sec,ms}=this.state;
         return(
