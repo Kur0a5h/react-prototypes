ed18732679d6afd992cf6f4b05836816d5a0b15d
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-20 09:25:56 -0800

    React | Styles and Images - Kuroash Esmaili

--- src/components/scrapbook.js
+++ src/components/scrapbook.js
@@ -1,11 +1,20 @@
 
 
 import  React from "react";
+import './scrapbook.css';
+import imageData from "./image_data";
+import ScrapbookImage from './scrapbook_image';
 
-export default ()=>{
+
+export default props=>{
+    const images=imageData.map((img,index)=>{
+        return (
+            <ScrapbookImage key={index} about={img}/>
+        )
+    });
     return(
         <div className="scrapbook-container">
-        this is the scrap container
+        {images}
         </div>
     )
 }
